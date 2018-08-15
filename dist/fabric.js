5e0171ed2a65f6f1df47d34191f919f57a939c4a
Alex Dekau <alex.potatofarms@gmail.com>
2018-08-15 14:49:35 -0400

    log whether or not there is a properties member of the object.

--- dist/fabric.js
+++ dist/fabric.js
@@ -311,7 +311,7 @@ fabric.Collection = {
     if (this._onObjectAdded) {
       for (var i = 0, length = arguments.length; i < length; i++) {
         this._onObjectAdded(arguments[i]);
-        console.log('object added', arguments[i]);
+        console.log('object added', arguments[i].hasOwnProperty('properties'));
       }
     }
     this.renderOnAddRemove && this.requestRenderAll();
